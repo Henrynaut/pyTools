601ab3a5a662ba1d93701ffe59aef246685ce657
Neil McHenry <neilmchenry@gmail.com>
2020-08-07 22:46:03 -0500

    Ensure even number of rats

--- genetic.py
+++ genetic.py
@@ -5,8 +5,12 @@
 
 #Breed Super Rats, with avg. weight of 110lbs
 
+import time
+import random
+import statistics
+
 #INPUT ASSUMPTIONS FOR SUPER-RATES GENETIC ALGORITHM
-Goal = 50000
+Goal = 50000    #weight in grams
 NUM_RATS = 20
 INITIAL_MIN_WT = 200
 INITIAL_MAX_WT = 600
@@ -17,6 +21,10 @@ MUTATE_MAX = 1.2
 LITTER_SIZE = 8
 LITTERS_PER_YEAR = 10
 GENERATION_LIMIT = 500
+
+# ensure even-number of rates for breeding pairs:
+if NUM_RATS % 2 != 0:
+    NUM_RATS += 1
 
     #Populate
     #Grade
