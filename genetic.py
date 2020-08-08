bd8136203fd5f864e0eea0a20a7b8cb81b6f379d
Neil McHenry <neilmchenry@gmail.com>
2020-08-07 22:39:56 -0500

    Added psuedocode

--- genetic.py
+++ genetic.py
@@ -2,3 +2,10 @@
 # about a problem, especially with a nonlinear problem
 # in which brute force methods are needed in a large
 # search space. They are easy to grasp and implement
+
+#Breed Super Rats
+    #Populate
+    #Grade
+    #Select
+    #Breed
+    #Mutate
