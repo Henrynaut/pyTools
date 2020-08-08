45c7ab88adef7f237b3956ae9225af1b025a32f8
Neil McHenry <neilmchenry@gmail.com>
2020-08-07 22:37:47 -0500

    Added genetic.py

---
+++ genetic.py
@@ -0,0 +1,4 @@
+#Genetic Algorithms are usefull when little is known
+# about a problem, especially with a nonlinear problem
+# in which brute force methods are needed in a large
+# search space. They are easy to grasp and implement
