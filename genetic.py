5ea47bea4e2f3151d5cfe9edb1269b20649c1190
Neil McHenry <neilmchenry@gmail.com>
2020-08-07 23:18:57 -0500

    Fixed typos

--- genetic.py
+++ genetic.py
@@ -34,12 +34,12 @@ def populate(num_rats, min_wt, max_wt, mode_wt):
 #Grade
 def fitness(population, goal):
     """Measure population fitness based on an attribute mean vs target."""
-    ave = statistcs.mean(population)
+    ave = statistics.mean(population)
     return ave / goal
 #Select
 def select(population, to_retain):
     """Cull a population to retain only a specified number of members."""
-    sorted population = sorted(population)
+    sorted_population = sorted(population)
     to_retain_by_sex = to_retain//2
     members_per_sex = len(sorted_population)//2
     females = sorted_population[:members_per_sex]
